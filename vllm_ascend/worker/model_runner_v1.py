9796f3d3dd0b8ebc310a6ceb4ce4f089b58d4116
natureofnature <wzliu@connect.hku.hk>
2025-09-12 17:50:19 +0800

    udpate

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -1278,7 +1278,8 @@ class NPUModelRunner(LoRAModelRunnerMixin):
         # so set seq_lens to current-step scheduled lengths to avoid inflating to
         # (computed + query). Keep original cumulative behavior for other cases.
         if self.cp_size > 1 and is_prefill:
-            self.seq_lens_np[:num_reqs] = num_scheduled_tokens
+            self.seq_lens_np[:num_reqs] = self.input_batch.num_computed_tokens_cpu[:num_reqs]
+            logger.info(f"===> seq_lens_np:{self.seq_lens_np[:num_reqs]}")
         else:
             self.seq_lens_np[:num_reqs] = (
                 self.input_batch.num_computed_tokens_cpu[:num_reqs] +
